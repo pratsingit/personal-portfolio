7cf62a8dfaca509fe7f199dea74724d39a8dd35b
pratsingit <pratyushchowdhury27@gmail.com>
2024-07-05 00:32:19 +0530

    Navbar finishing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://www.mcciapunesampada.com/2024/06/exploring-disk-forensics-for-digital.html"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/pratsingit/personal-portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
